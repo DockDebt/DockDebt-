489d318c1eeb96a8c76ba88557a3bfe183943faf
DockDebt <crossbonesjoe42@gmail.com>
2026-01-17 15:16:25 -0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
-import { defineConfig } from 'vite';
+uild: {
+    target: 'esnext'
+  import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   define: {
     'process.env.API_KEY': JSON.stringify(process.env.API_KEY || '')
-  }
+  },
+  b}
 });
